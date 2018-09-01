63b2f9bffc02df3abc86bf9ee9624ee07a173680
toddlerya <toddlerya@gmail.com>
2018-09-01 21:23:32 +0800

    insert file md5

--- managedata/db.go
+++ managedata/db.go
@@ -2,20 +2,29 @@ package managedata
 
 import (
 	"database/sql"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var dbPath string = "data/data.db"
 
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func InitSqlite() {
 	// 连接数据库
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
-		log.Fatal(err)
+		checkErr(err)
 	}
 	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		checkErr(err)
+	}
 
 	// 建表
 	createStudentsBaseInfoSql := `CREATE TABLE IF NOT EXISTS students_base_info (
@@ -26,12 +35,17 @@ func InitSqlite() {
 		stu_grade VARCHAR(64),
 		stu_class VARCHAR(64)
 	)`
+	createUniqIndexStudentsBaseInfoSql := `
+	CREATE UNIQUE INDEX IF NOT EXISTS stu_idx ON students_base_info(stu_code, stu_name)`
 
 	_, err = db.Exec(createStudentsBaseInfoSql)
-	if err != nil {
-		log.Panicf("%q: %s\n", err, createStudentsBaseInfoSql)
-		return
-	}
+	checkErr(err)
+	_, err = db.Exec(createUniqIndexStudentsBaseInfoSql)
+	checkErr(err)
+	//	if err != nil {
+	//		log.Panicf("%q: %s\n", err, createStudentsBaseInfoSql)
+	//		return
+	//	}
 
 	createXlsxInfoSql := `CREATE TABLE IF NOT EXISTS xlsx_info (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -40,34 +54,74 @@ func InitSqlite() {
 		xlsx_date VARCHAR(64),
 		xlsx_size VARCHAR(64)
 	)`
+	createUniqIndexXlsxInfoSql := `
+	CREATE UNIQUE INDEX IF NOT EXISTS  xlsx_idx ON xlsx_info (xlsx_name, xlsx_md5)`
 
 	_, err = db.Exec(createXlsxInfoSql)
-	if err != nil {
-		log.Panicf("%q: %s\n", err, createXlsxInfoSql)
-		return
-	}
+	checkErr(err)
+	_, err = db.Exec(createUniqIndexXlsxInfoSql)
+	checkErr(err)
+	//	if err != nil {
+	//		log.Panicf("%q: %s\n", err, createXlsxInfoSql)
+	//		return
+	//	}
 }
 
-func InsertData2db(data []map[string]string) {
+func InsertXlsxData2db(data []map[string]string) {
 	db, err := sql.Open("sqlite3", dbPath)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 	defer db.Close()
 
 	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-	stmt, err := tx.Prepare("insert into students_base_info(stu_code, stu_name, stu_sex, stu_class, stu_grade) values(?, ?, ?, ?, ?)")
+	checkErr(err)
+	stmt, err := tx.Prepare("insert or replace into students_base_info(stu_code, stu_name, stu_sex, stu_class, stu_grade) values (?, ?, ?, ?, ?)")
 	defer stmt.Close()
 
 	for _, row := range data {
 		_, err = stmt.Exec(row["stu_code"], row["stu_name"], row["stu_sex"], row["stu_class"], row["stu_grade"])
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkErr(err)
 	}
 	tx.Commit()
+}
+
+func InsertXlsxInfo(xlsMap map[string]string) {
+	db, err := sql.Open("sqlite3", dbPath)
+	checkErr(err)
+	defer db.Close()
+
+	tx, err := db.Begin()
+	checkErr(err)
+
+	stmt, err := tx.Prepare("insert or replace into xlsx_info (xlsx_name, xlsx_md5, xlsx_date, xlsx_size ) values (?, ?, ?, ?)")
+	checkErr(err)
+	defer stmt.Close()
+
+	_, err = stmt.Exec(xlsMap["xlsx_name"], xlsMap["xlsx_md5"], xlsMap["xlsx_date"], xlsMap["xlsx_size"])
+	checkErr(err)
+
+	tx.Commit()
+}
+
+func QueryXlsxInfo(xlsxName string) (map[string]string, int) {
+	db, err := sql.Open("sqlite3", dbPath)
+	checkErr(err)
+	defer db.Close()
+
+	var xlsxMd5 string
+	var xlsxDate string
+	var xlsxSize string
+	m := make(map[string]string)
+	err = db.QueryRow("select xlsx_md5, xlsx_date, xlsx_size from xlsx_info where xlsx_name=?", xlsxName).Scan(&xlsxMd5, &xlsxDate, &xlsxSize)
+	switch {
+	case err == sql.ErrNoRows:
+		return m, 0
+	case err != nil:
+		checkErr(err)
+	}
+	m["xlsx_name"] = xlsxName
+	m["xlsx_md5"] = xlsxMd5
+	m["xlsx_date"] = xlsxDate
+	m["xlsx_size"] = xlsxSize
+	return m, 1
 }
--- managedata/loaddata.go
+++ managedata/loaddata.go
@@ -1,16 +1,31 @@
 package managedata
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
+
+func calcMd5(fileName string) string {
+	fileObj, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	md5hash := md5.New()
+	io.Copy(md5hash, fileObj)
+	fileMd5 := hex.EncodeToString(md5hash.Sum(nil))
+	return fileMd5
+}
 
 // 获取指定目录下所有的xlsx文件名
 func getAllXlsx(dirpath string) ([]string, error) {
@@ -48,7 +63,7 @@ func readXlsx(xlsxPath string) ([][]string, error) {
 }
 
 // 解析所有数据，进行预处理, 应该是根据每个文件的md5判断, 若文件发生变更则根据此文件更新数据库
-func parseData(dataPath string) ([]map[string]string, error) {
+func parseXlsxData(dataPath string) ([]map[string]string, error) {
 	var datas []map[string]string
 	// 获取所有的文件
 	allFiles, err := getAllXlsx(dataPath)
@@ -59,13 +74,45 @@ func parseData(dataPath string) ([]map[string]string, error) {
 
 	// 依次读取所有文件
 	for _, filePathName := range allFiles {
+		// 获取文件去除路径后的名字
+		xlsxName := filepath.Base(filePathName)
+		xlsxName = strings.TrimSpace(xlsxName)
+		// 校验文件是否有改变, 若无改变则不在重新读取
+		fileInfoMap, num := QueryXlsxInfo(xlsxName)
+		fmt.Println("===>", num)
+		fmt.Println(fileInfoMap)
+		if num == 1 {
+			oldFileMd5 := fileInfoMap["xlsx_md5"]
+			newFileMd5 := calcMd5(filePathName)
+			if newFileMd5 == oldFileMd5 {
+				fmt.Printf("%s文件没有变化, 跳过, md5: %s", filePathName, oldFileMd5)
+				continue
+			}
+		} else {
+			// 数据库没有记录此文件, 获取文件信息入库
+			m := make(map[string]string)
+			xlsxMd5 := calcMd5(xlsxName)
+			xlsxBaseInfo, err := os.Stat(filePathName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			xlsxDate := xlsxBaseInfo.ModTime()
+			xlsx_unix_sec, _ := time.ParseInLocation("2006-01-02 15:04:05", xlsxDate, time.Local)
+			xlsxSize := xlsxBaseInfo.Size()
+			fmt.Println(xlsxDate)
+			fmt.Println(xlsx_unix_sec)
+			fmt.Println(xlsxSize)
+			m["xlsx_name"] = xlsxName
+			m["xlsx_md5"] = xlsxMd5
+			m["xlsx_date"] = fmt.Sprintf("%s", xlsx_unix_sec)
+			m["xlsx_size"] = fmt.Sprintf("%s", xlsxSize)
+			InsertXlsxInfo(m)
+		}
 		xlsxArray, err := readXlsx(filePathName)
 		if err != nil {
 			log.Printf("读取%s错误: %s", filePathName, err)
 		}
 		// 获取班级信息
-		xlsxName := filepath.Base(filePathName)
-		xlsxName = strings.TrimSpace(xlsxName)
 		xlsxSplitArray := strings.Split(xlsxName, ".xlsx")[0]
 		tmpArray := strings.Split(xlsxSplitArray, "级")
 		grade := tmpArray[0]
@@ -75,7 +122,6 @@ func parseData(dataPath string) ([]map[string]string, error) {
 		if err != nil {
 			log.Fatalf("请检查xlsx文件名是否为XXXX级Y班, XXXX为年份(比如2018代表2018级), Y为班级序号(比如1代表一班)")
 		}
-		//		fmt.Printf("文件: %s, 年级: %s, 班级: %s, 内容: %s \n", xlsxName, grade, class, xlsxArray)
 		for _, item := range xlsxArray {
 			m := make(map[string]string)
 			m["stu_code"] = item[0]
@@ -90,10 +136,10 @@ func parseData(dataPath string) ([]map[string]string, error) {
 }
 
 func LoadData() {
-	datas, err := parseData("data")
+	InitSqlite()
+	xlsxDatas, err := parseXlsxData("data")
 	if err != nil {
 		log.Fatal(err)
 	}
-	InitSqlite()
-	InsertData2db(datas)
+	InsertXlsxData2db(xlsxDatas)
 }
