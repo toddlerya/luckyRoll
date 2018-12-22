68fa5ab8c43c5fe208fbe92b7ca7b0acc94de824
toddlerya <toddlerya@gmail.com>
2018-12-22 11:55:52 +0800

    将开始停止按钮合并为一个

--- web/static/js/roll.js
+++ web/static/js/roll.js
@@ -1,16 +1,33 @@
 var lucky;
+var goStatus = true;
 
 function randomChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
+
+function roll(nameArray) {
+    if (goStatus) {
+        startRoll(nameArray);
+    } else {
+        stopRoll();
+    }
+    goStatus = !goStatus;
+
+}
+
 function startRoll(nameArray) {
     var displayName = document.getElementById("content");
+    var goButton = document.getElementById("go");
+    goButton.innerHTML = "停止";
     lucky = setInterval(function() {
         displayName.innerHTML = randomChoice(nameArray)["Name"];
     }, 50);
+
 }
 
 function stopRoll() {
+    var goButton = document.getElementById("go");
+    goButton.innerHTML = "开始";
     clearInterval(lucky);
 }
